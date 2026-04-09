00144e2f63427823e4e2208f19480e023c9dc768
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2026-04-09 01:40:18 +0000

    Enhance mobile navigation functionality and improve overlay behavior

--- js/script.js
+++ js/script.js
@@ -2,31 +2,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const burger = document.getElementById("burger");
 const mobileNav = document.getElementById("mobileNav");
+const overlay = document.getElementById("overlay");
 
+
+// TOGGLE MENU
 if (burger && mobileNav){
 burger.addEventListener("click", () => {
 mobileNav.classList.toggle("active");
+
+if (overlay){
+overlay.classList.toggle("active");
+}
+
 });
 }
 
+
+// CLOSE WHEN CLICK OVERLAY
+if (overlay){
+overlay.addEventListener("click", () => {
+mobileNav.classList.remove("active");
+overlay.classList.remove("active");
+});
+}
+
+
+// CLOSE WHEN CLICK OUTSIDE
+document.addEventListener("click",(e)=>{
+
+if(
+mobileNav &&
+burger &&
+!mobileNav.contains(e.target) &&
+!burger.contains(e.target)
+){
+mobileNav.classList.remove("active");
+
+if (overlay){
+overlay.classList.remove("active");
+}
+}
+
+});
+
+
+// FAQ
 document.querySelectorAll(".faq-question").forEach(btn=>{
 btn.addEventListener("click",()=>{
 btn.parentElement.classList.toggle("active")
 })
-})
-
-document.addEventListener("click",(e)=>{
-
-const burger = document.getElementById("burger");
-const nav = document.getElementById("mobileNav");
-
-if(!nav.contains(e.target) && !burger.contains(e.target)){
-nav.classList.remove("active");
-}
-
 });
 
-// force render icons
+
+// LUCIDE
 if (window.lucide) {
 lucide.createIcons();
 }
